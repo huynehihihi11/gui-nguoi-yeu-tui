ffbadb29008148c11bfebaa50f84948b5353d2f7
huynehihihi11 <87898217+huynehihihi11@users.noreply.github.com>
2021-07-24 17:47:51 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
     introTitle: 'Bae à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
+    introDesc: `Ngay từ lúc mà em nhấn vào link này thì e đã là người yêu anh rồi đó nhaa ( ͡° ͜ʖ ͡°)`,
     btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
+    title: 'Phải chăng anh đã yêu ngay từ cái nhìn đầu tiên 😙',
+    desc: 'Phải chăng anh đã say ngay từ lúc thấy nụ cười ấy ',
     btnYes: 'Vẫn cứ thích anh ',
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ nụ cười nhưng nụ cười của em là đẹp nhất',
     btnReply: 'Gửi cho anh <3',
     reply: 'EM YÊU ANHHHH ',
-    mess: 'Anh biết mà.Yêu em nhiều nhiều 😘',
+    mess: 'Anh biết mà, yêu em nhiều nhiều 😘',
     messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'OKE LUÔNNNN ',
+    btnAccept: 'dạ anh yêu 😍',
     messLink: 'https://www.facebook.com/HHH.HHH.HHHUyy/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
