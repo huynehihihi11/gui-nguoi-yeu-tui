f523af91b766cf45b27f1eb1faf1875d26697e31
huynehihihi11 <87898217+huynehihihi11@users.noreply.github.com>
2021-07-24 18:04:22 +0700

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,11 @@ const CONFIG = {
     title: 'Trên đời này, có ba thứ làm cho anh mê muội, chìm đắm không lối thoát,đó là: tiền, đồ ăn ngon và EM.',
     btnYes: 'Vẫn cứ thích anh ',
     btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ nụ cười nhưng nụ cười của em là đẹp nhất',
+    question:'Nói với anh vài lời được không ?',
     btnReply: 'Gửi cho anh <3',
-    reply: 'EM YÊU ANHHHH ',
+    reply: 'Em vẫn chỉ muốn nói với anh ” Em Yêu Anh” ',
     mess: 'Anh biết mà, yêu em nhiều nhiều 😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
+    messDesc: 'Tối nay 7h anh qua đón em  nhé công chúa.',
     btnAccept: 'dạ anh yêu 😍',
     messLink: 'https://www.facebook.com/HHH.HHH.HHHUyy/' //link mess của các bạn. VD: https://m.me/nam.nodemy
 }
